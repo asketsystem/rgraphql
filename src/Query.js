6a94d2e9c411b14779d21f1cf574c8fcc1925b08
asketsystem <asketsystem1@gmail.com>
2022-02-04 16:41:55 +0000

    Creating dynamic queries

--- src/Query.js
+++ src/Query.js
@@ -1,25 +1,28 @@
-const githubQuery = {
+const githubQuery = (pageCount, queryString) => {
+    return {
     query: `
-    {
-        viewer {
-            name
-        }
-          search(query: "user:asketsystem sort:updted-desc", type: REPOSITORY, first: 10) {
-            nodes {
-              ... on Repository {
-                    name
-                    description
-                    id
-                    url
-                    viewerSubscription
-                    licenseInfo {
-                        spdxId
+        {
+            viewer {
+                name
+            }
+            search(query: "${queryString}user:asketsystem sort:updted-desc", type: REPOSITORY, first: 10) {
+                repositoryCount
+                nodes {
+                ... on Repository {
+                        name
+                        description
+                        id
+                        url
+                        viewerSubscription
+                        licenseInfo {
+                            spdxId
+                        }
                     }
                 }
             }
         }
-    }
     `,
+    };
 };
 
   export default githubQuery
